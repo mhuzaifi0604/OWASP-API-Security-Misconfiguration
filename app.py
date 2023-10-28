f4950638686b6cb7aa258cc49aa7938230239cda
mhuzaifi0604 <huzzaifaasim@gmail.com>
2023-10-29 03:34:53 +0500

    Development & Testing Done

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import datetime
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "http://localhost:5500"}})
+CORS(app, resources={r"/*": {"origins": "*"}})
 app.config['SECRET_KEY'] = 'Security Misconfig 420'
 
 users = []
@@ -32,7 +32,7 @@ def signup():
         "password": password,
         "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
     }
-    token = jwt.encode(payload, app.config['SECRET_KEY'], algorithm='HS256')
+    token = jwt.encode(payload, app.config['SECRET_KEY'], algorithm=allowed_algorithms[1])
 
     print(f"Received data - Username: {username}, Password: {password}")
     
@@ -45,7 +45,7 @@ def validate():
     try:
         decoded = jwt.decode(token, options={"verify_signature": False}, algorithms=allowed_algorithms)
         print(decoded)
-        return jsonify({'message': 'Valid token'})
+        return jsonify({'message': 'Valid token && User Validated Successfully!'})
     except jwt.ExpiredSignatureError:
         return jsonify({'message': 'Token expired'})
 
